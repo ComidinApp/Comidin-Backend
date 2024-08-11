3808ea5678c2b0c0b5f62422c2aab872c7eeca2c
Juan Pablo Donalisio <jpdona@hotmail.com>
2024-08-11 20:45:11 -0300

    changes in all controllers

--- src/controllers/commerce.js
+++ src/controllers/commerce.js
@@ -1,71 +1,71 @@
 
-const User = require('../models/user');
+const Commerce = require('../models/commerce');
 
-const createUser = async (req, res) => {
+const createCommerce = async (req, res) => {
     try {
         const { body } = req;
-        const user = new User(body);
-        await user.save();
-        return res.status(201).json(user);
+        const commerce = new Commerce(body);
+        await commerce.save();
+        return res.status(201).json(commerce);
     } catch (error) {
         console.log(error)
         res.status(500).json({ error: error.message });
     }
 }
 
-const findAllUsers = async (req, res) => {
+const findAllCommerces = async (req, res) => {
     try {
-        const users = await User.findAll()
-        return res.status(200).json(users);
+        const commerces = await Commerce.findAll()
+        return res.status(200).json(commerces);
     } catch (error) {
         console.log(error)
         return res.status(500).json({ error: error.message });
     }
 }
 
-const findUserById = async (req, res) => {
+const findCommerceById = async (req, res) => {
     try {
         const { id } = req.params;
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({ error: 'User not found with id:' + id });
+        const commerce = await Commerce.findByPk(id);
+        if (!commerce) {
+            return res.status(404).json({ error: 'commerce not found with id:' + id });
         }
-        return res.status(200).json(user);
+        return res.status(200).json(commerce);
     } catch (error) {
         console.log(error)
         return res.status(500).json({ error: error.message });
     }
 }
 
-const updateUser = async (req, res) => {
+const updateCommerce = async (req, res) => {
     try {
         const { body } = req;
         const { id } = req.params;
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({ error: 'User not found with id:' + id });
+        const commerce = await Commerce.findByPk(id);
+        if (!commerce) {
+            return res.status(404).json({ error: 'commerce not found with id:' + id });
         }
-        await user.update(body);
-        return res.status(201).json(user);
+        await commerce.update(body);
+        return res.status(201).json(commerce);
     } catch (error) {
         console.log(error)
         return res.status(500).json({ error: error.message });
     }
 }
 
-const deleteUser = async (req, res) => {
+const deleteCommerce = async (req, res) => {
     try {
         const { id } = req.params;
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({ error: 'User not found with id:' + id });
+        const commerce = await Commerce.findByPk(id);
+        if (!commerce) {
+            return res.status(404).json({ error: 'commerce not found with id:' + id });
         }
-        await user.destroy()
-        return res.status(200).json(user);
+        await commerce.destroy()
+        return res.status(200).json(commerce);
     } catch (error) {
         console.log(error)
         return res.status(500).json({ error: error.message });
     }
 }
 
-module.exports =  {createUser, updateUser, deleteUser, findAllUsers, findUserById};
+module.exports =  {createCommerce, updateCommerce, deleteCommerce, findAllCommerces, findCommerceById};
