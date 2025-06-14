58f844c9ab8a9bd959bf7b6ba18d107947f956e9
Juan Pablo Donalisio <38335384+JuanDonalisio@users.noreply.github.com>
2025-06-14 13:11:47 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -79,6 +79,6 @@ const init = async () => {
 
 init();
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
